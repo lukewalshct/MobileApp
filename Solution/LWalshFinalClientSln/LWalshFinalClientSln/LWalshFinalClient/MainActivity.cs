5db71deb4f50c9edf2eb60a237577ae9a8b06221
Luke Walsh <lukewalshct@gmail.com>
2016-04-19 23:59:36 -0400

    started adding FB authentication

--- Solution/LWalshFinalClientSln/LWalshFinalClientSln/LWalshFinalClient/MainActivity.cs
+++ Solution/LWalshFinalClientSln/LWalshFinalClientSln/LWalshFinalClient/MainActivity.cs
@@ -11,6 +11,8 @@ namespace LWalshFinalClient
     [Activity(Label = "LWalshFinalClient", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        public MobileServiceClient client;
+
         Button loginButton;
         Button quitButton;
         bool isLoggedIn;
@@ -40,7 +42,40 @@ namespace LWalshFinalClient
 
         private void loginButtonClick(Object sender, EventArgs e)
         {
+            if (client.CurrentUser == null || client.CurrentUser.UserId == null)
+            {
+                this.isLoggedIn = true;
+                MobileServiceAuthenticationProvider providerType;
 
+                if (this.loginTwitter.Checked)
+                {
+                    providerType = MobileServiceAuthenticationProvider.Twitter;
+                }
+                else if (this.loginGoogle.Checked)
+                {
+                    providerType = MobileServiceAuthenticationProvider.Google;
+                }
+                else if (this.loginFacebook.Checked)
+                {
+                    providerType = MobileServiceAuthenticationProvider.Facebook;
+                }
+                else
+                {
+                    providerType = MobileServiceAuthenticationProvider.MicrosoftAccount;
+                }
+
+                await AuthenticateUserAsync(providerType);
+            }
+            else
+            {
+                this.isLoggedIn = false;
+                this.isProfileView = false;
+                this.remainingVotesValue.Text = "";
+                await client.LogoutAsync();
+            }
+            await saveCurrentItem();
+            refreshFeaturesList();
+            updateDisplay(null);
         }
     }
 }
